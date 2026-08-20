36837e27c410374bee6adfbc7e72da443346525f
Azumi RH <contato@azumirh.com.br>
2026-08-20 16:40:08 -0300

    chore: centralize Ken Taki platform branding

---
+++ src/lib/brand.ts
@@ -0,0 +1,6 @@
+export const BRAND = {
+  name: "Ken Taki",
+  product: "Intranet",
+  footerCredit: "By Patrícia Lima · Calia RH • Azumi RH",
+  emailFrom: "Ken Taki <no-reply@azumirh.com.br>",
+} as const;
